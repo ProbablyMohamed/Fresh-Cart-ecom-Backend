06b8311910bfcf9ea10d0e77d8a9effeeb5253a9
ProbablyMohamed <mohamedkhaledkhaled123438@yahoo.com>
2024-09-12 02:38:34 +0300

    edit routes

--- src/modules/order/order.routes.js
+++ src/modules/order/order.routes.js
@@ -6,6 +6,6 @@ const router = Router();
 router.post('/:id', protectedRoutes, allowedTo('user','admin') , orderController.createCashOrder)
 .get('/',protectedRoutes,allowedTo('admin') , orderController.getAllOrders)
 .get('/users',protectedRoutes,allowedTo('admin','user') , orderController.getUserOrders)
-.post('/checkout/:id',protectedRoutes,allowedTo('user') , orderController.createCheckoutSession)
+.post('/checkout/:id',protectedRoutes, orderController.createCheckoutSession)
 
 export default router
