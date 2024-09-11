7ebffb2b879577a4fddedef5d7f9e746479f028e
ProbablyMohamed <mohamedkhaledkhaled123438@yahoo.com>
2024-09-12 02:33:30 +0300

    stripe added

--- index.js
+++ index.js
@@ -3,11 +3,40 @@ import mongoose from "mongoose";
 import cors from "cors";
 import bootstrap from './src/bootstrap.js'
 import globalError from "./src/middleware/globalError.js";
-
+import Stripe from 'stripe';
+const stripe = new Stripe('sk_test_51PxweoG9uriXVRF0qgFFtIyarUzSSNzx3ezgZuyWbwxzAa0Fp5xBqY4PI36e6M0myqBYBU3eO9EBqRXLhYX3Pci600p8MTAUyf');
 
 
 const app = express();
 const port = process.env.PORT ||3000; 
+app.post('/api/webhook', (req, res) => {
+try {
+    const sig=req.headers['stripe-signature'].toString();
+    let event=stripe.webhooks.constructEvent(request.body,sig,"whsec_srEs1LU52PqwKOenM8OTISBbKJz96xM5");
+    let checkout
+if(event.type==='checkout.session.completed'){
+ checkout=event.data.object;
+}
+
+
+res.json({message:"success",checkout})
+
+
+
+} catch (error) {
+    new AppError(error.message, 500);
+}
+
+
+
+})
+
+
+
+
+
+
+
 app.use(cors());
 bootstrap(app, express);
 app.get("/", (req, res) => {
